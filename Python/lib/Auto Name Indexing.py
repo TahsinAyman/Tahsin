db929bc47c8948e5f330c24c89668f8aa922176a
Tahsin Ayman <mail4tahsin@gmail.com>
2022-02-04 23:06:26 +0600

    Updated

--- Python/lib/Auto Name Indexing.py
+++ Python/lib/Auto Name Indexing.py
@@ -1,11 +1,24 @@
-def name_indexing(names: list):
-    return ''
+"""
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+"""
+letter = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+          'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+          'z': 26}
+text = []
+result = []
 
+while True:
+    txt = input().lower()
+    if len(txt) > 0:
+        text.append(txt)
+    else:
+        break
+lst = []
+for i in text:
+    lst.append(letter[i[0]])
+lst.sort()
 
-if __name__ == '__main__':
-    names = []
-    for i in range(int(input())):
-        name = input()
-        names.append(name)
-
-    print(name_indexing(names))
+for i in lst:
+    for y in text:
+        pass
+print(result)
