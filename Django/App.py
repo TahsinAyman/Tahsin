0d69397a35d8da15bd18f7007f2bbe9732e04204
Rashed Karim <rashedmkarim@gmail.com>
2022-02-27 09:49:41 +0600

    Updated

--- Django/App.py
+++ Django/App.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-responce = requests.get('http://127.0.0.1:6969/playground/server')
+responce = requests.get('http://127.0.0.1:6969/playground/data')
 data = json.loads(responce.text)
 print(data)
---
+++ Django/open_server.cmd
@@ -0,0 +1,1 @@
+start msedge http://127.0.0.1:6969/playground/data
