be47d96ece8ba228d90c31c18afd9a2ac7205675
TahsinAyman <mail4tahsin@gmail.com>
2022-04-26 11:21:16 +0600

    Updated

--- Python/lib/Test.py
+++ Python/lib/Test.py
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
-from threading import Thread
-
-def pillow_pass(n):
-    lst = [_ for _ in range(1, n+1)]
-    while True:
-        if len(lst) == 1:
-            return lst[0]
-
-if __name__ == '__main__':
-    pass
-=======
->>>>>>> 160754ab82ef07eb35ef62546fbb05416123465d
+for i in range(2):
+    print('s')
--- Python/root/App.py
+++
@@ -1,2 +0,0 @@
-string = input('Github Test: ')
-print(string)
